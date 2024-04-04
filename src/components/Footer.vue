4da88d384b484de670edeb7ec673155202ecd950
Кинсфатор Дмитрий Эдуардович <kinsfator-de@college.tepk.dev>
2024-04-04 11:14:14 +0700

    Учимся

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    Barmaley & Barabas,2024
+  </div>
+  <div>
+    <a href="tel:8812525525">+7(812) 525-52-52</a>
+  </div>
+</template>
+<style scoped>
+div{
+  display: flex;
+  background: blueviolet;
+  justify-content: space-between;
+  padding: 20px ;
+  color: aliceblue;
+  text-transform: uppercase;
+}
+div a{
+  color:aliceblue;
+  font-weight: bold;
+}
+</style>
+<script>
+</script>
